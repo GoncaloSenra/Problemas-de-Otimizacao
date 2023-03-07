25dea616b9a77f5214d6f6a073f63cf708c1034d
GoncaloSenra <goncalosenra@gmail.com>
2023-03-07 16:56:22 +0000

    New isValid

--- A.cpp
+++ A.cpp
@@ -1,80 +1,539 @@
+
+/*
+    Gonçalo Senra   nº 2020213750
+    Henrique Costa  nº 2020214120
+*/
+
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
-class node {
-    public:
-    int dim;
-    vector<vector<int>> qr;
 
-    vector<node*> children;
+vector<vector<int>> saved;
 
-    node(int dim) : dim(dim), qr(dim, vector<int>(dim)) {}
+int countqrs = 0;
 
-    void newChild(node * child) {
-        children.push_back(child);
+void printQrCode(vector<vector<int>> qr, int dim) {
+
+    for (int i = 0; i < dim + 2; i++) {
+        for (int j = 0; j < dim + 2; j++) {
+            if (i == 0 || i == dim +1){
+                if (j == 0) {
+                    cout << "+";
+                } else if (j == dim + 1) {
+                    cout << "+\n";
+                } else {
+                    cout << "-";
+                }
+            } else {
+                if (j == 0) {
+                    cout << "|";
+                } else if (j == dim +1) {
+                    cout << "|\n";
+                } else {
+                    if (qr[i-1][j-1] == 0) {
+                        cout << " ";
+                    } else if (qr[i-1][j-1] == 1) {
+                        cout << "#";
+                    }
+                }
+            }
+        }
     }
 
-};
+}
 
-void fill_line(node * root, vector<int> vec, int n, int blacks, int i, int line);
+//TODO: OTIMIZAR QUADRANTES e DIAGONAIS
+bool isValid(vector<vector<int>> qr, int dim, int line, int col, vector<int> *lb, vector<int> *cb, vector<int> *lt, vector<int> *ct, vector<int> *qb, vector<int> *db) {
 
+    int tam, tam2;
+
+    if (dim % 2 == 0) 
+        tam = (dim / 2); tam2 = (dim / 2);
+    if (dim % 2 != 0) 
+        tam = (dim / 2); tam2 = (dim / 2) + 1;
+
+    int aux1 = tam * tam2 , aux2 = tam * tam, aux3 = tam * tam2, aux4 = tam2 * tam2;
+
+    if (line == 0 && col == 0)
+        return true;
+
+    if (col > 0) {
+        if (qr[line][col - 1] == 1) {
+            (*lb)[line]--;
+            (*cb)[col - 1]--;
+            if (line == col - 1) {
+                (*db)[0]--;
+            }
+            if (line + col == dim) {
+                (*db)[1]--;
+            }
+
+            if (line <= (dim / 2) - 1 && col - 1 <= (dim / 2) - 1) {
+                (*qb)[1]--;
+            } else if (line <= (dim / 2) - 1 && col - 1 > (dim / 2) - 1) {
+                (*qb)[0]--;
+            } else if (line > (dim / 2) - 1 && col - 1 <= (dim / 2) - 1) {
+                (*qb)[2]--;
+            } else if (line > (dim / 2) - 1 && col - 1 > (dim / 2) - 1) {
+                (*qb)[3]--;
+            }
+        }
+        if (col > 1) {
+            if (qr[line][col - 2] != qr[line][col - 1]) {
+                (*lt)[line]--;
+            }
+        }
+        if (line > 0) {
+            if (qr[line][col - 1] != qr[line - 1][col - 1]) {
+                (*ct)[col - 1]--;
+            }
+        }
+    } else {
+        if (qr[line - 1][dim - 1] == 1) {
+            (*lb)[line - 1]--;
+            (*cb)[dim - 1]--;
+            if (line - 1 == dim - 1) {
+                (*db)[0]--;
+            }
+            if ((line - 1) + (dim) == dim) {
+                (*db)[1]--;
+            }
+
+            if ((line - 1) <= (dim / 2) - 1 && dim - 1 <= (dim / 2) - 1) {
+                (*qb)[1]--;
+            } else if ((line - 1) <= (dim / 2) - 1 && dim - 1 > (dim / 2) - 1) {
+                (*qb)[0]--;
+            } else if ((line - 1) > (dim / 2) - 1 && dim - 1 <= (dim / 2) - 1) {
+                (*qb)[2]--;
+            } else if ((line - 1) > (dim / 2) - 1 && dim - 1 > (dim / 2) - 1) {
+                (*qb)[3]--;
+            }
+        }
+        
+        if (qr[line - 1][dim - 2] != qr[line - 1][dim - 1]) {
+            (*lt)[line - 1]--;
+        }
+        
+        if (line > 1) {
+            if (qr[line - 1][dim - 1] != qr[line - 2][dim - 1]) {
+                (*ct)[dim - 1]--;
+            }
+        }
+
+        //NOTE: FINAL DE LINHA
+        if ((*lb)[line - 1] != 0 || (*lt)[line - 1] != 0) {
+            return false;
+        }
+
+    }
+    /*
+    //NOTE: LIVE LINHAS
+    if ((*lb)[line] < 0 || (*lt)[line] < 0) {
+        return false;
+    }  
+    //NOTE: LIVE COLUNAS
+    if (col > 0) {
+        if ((*cb)[col - 1] < 0 || (*ct)[col - 1] < 0) {
+            return false;
+        }  
+    }*/
+
+    //NOTE: LIVE COUNTING
+    if (line != dim) {
+        if ((*lb)[line] < 0 || (*lt)[line] < 0 || (*cb)[col - 1] < 0 || (*ct)[col - 1] < 0 || (*db)[0] < 0 || (*db)[1] < 0 || (*qb)[0] < 0 || (*qb)[1] < 0 || (*qb)[2] < 0 || (*qb)[3] < 0) {
+            return false;
+        }
+        //NOTE: COUNT WHITES
+        if (col > 0 && dim - col < (*lb)[line]){
+            return false;
+        }
+        if (col > 0 && dim - line < (*cb)[col - 1]) {
+            return false;
+        }
+    }
+
+
+    //NOTE: FINAL DE COLUNA
+    if (line == dim - 1 && col > 0 ) {
+        if ((*cb)[col - 1] != 0 || (*ct)[col - 1] != 0) {
+            return false;
+        }
+    }
+
+    //NOTE: FINAL DE CONTRA-DIAGONAL
+    if (line == dim - 1 && col == 1){
+        if ((*db)[1] != 0) {
+            return false;
+        }
+    }
+
+    //NOTE: FINAL DA MATRIZ
+    if (line == dim) {
+        if ((*cb)[dim - 1] != 0 || (*db)[0] != 0 || (*qb)[0] != 0 || (*qb)[1] != 0 || (*qb)[2] != 0 || (*qb)[3] != 0 || (*ct)[dim - 1] != 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+bool rec(vector<vector<int>> qr, vector<vector<int>> visited, int dim, int i, int j, vector<int> lb, vector<int> cb, vector<int> lt, vector<int> ct, vector<int> qb, vector<int> db) {
+    
+    /*
+    if (qr[0][0] == 1 && qr[0][1] == 0 && qr[0][2] == 1 && qr[0][3] == 0){
+        cout << "debug";
+        if (qr[1][0] == 1 && qr[1][1] == 0 && qr[1][2] == 1 && qr[1][3] == 0){
+            cout << "";
+            if (qr[2][0] == 0 && qr[2][1] == 1 && qr[2][2] == 0 && qr[2][3] == 1){
+                cout << "";
+                if (qr[3][0] == 0 && qr[3][1] == 1 && qr[3][2] == 0 && qr[3][3] == 1){
+                    cout << "";
+                }
+            }
+        }
+    }
+    */
+    if (i == dim) {
+        i = 0;
+        j++;
+    }
+    
+    if (!isValid(qr, dim, j, i, &lb, &cb, &lt, &ct, &qb, &db)){
+        return false;
+    }
+
+    if (j == dim) {
+        saved = qr;
+        countqrs++;
+        return true;
+    }
+
+    if (visited[j][i] == 0) {
+        qr[j][i] = 1;
+        visited[j][i] = 1;
+        rec(qr, visited, dim, i + 1, j, lb, cb, lt, ct, qb, db);
+        qr[j][i] = 0;
+        visited[j][i] = 0;
+        rec(qr, visited, dim, i + 1, j, lb, cb, lt, ct, qb, db);
+    } else {
+        rec(qr, visited, dim, i + 1, j, lb, cb, lt, ct, qb, db);
+    }
+    
+    return true;
+}
+
+void pre_proc(vector<vector<int>> *qr, vector<vector<int>> *visited, int n, vector<int> lb, vector<int> cb, vector<int> lt, vector<int> ct, vector<int> qb, vector<int> db) {
+    
+    int countLines1 = 0; 
+    int countCols1 = 0; 
+    int countLines0 = 0; 
+    int countCols0 = 0;
+
+    int countDiagonal1 = 0; 
+    int countDiagonal0 = 0;
+    int countDiagonalInv1 = 0; 
+    int countDiagonalInv0 = 0;
+
+
+    vector<int> idxLines(n);
+    vector<int> idxCols(n);
+
+    //NOTE: FILL LINES AND COLS
+    for (int i = 0; i < n; i++) {
+        if (lb[i] == 0) {
+            idxLines[i] = 1;
+            countLines0++;
+            for (int j = 0; j < n; j++) {
+                (*visited)[i][j] = -1;
+            }
+        }
+        if (cb[i] == 0) {
+            idxCols[i] = 1;
+            countCols0++;
+            for (int j = 0; j < n; j++) {
+                (*visited)[j][i] = -1;
+            }
+        }
+        if (lb[i] == n) {
+            idxLines[i] = 2;
+            countLines1++;
+            for (int j = 0; j < n; j++) {
+                (*qr)[i][j] = 1;
+                (*visited)[i][j] = 1;
+            }
+        }
+        if (cb[i] == n) {
+            idxCols[i] = 2;
+            countCols1++;
+            for (int j = 0; j < n; j++) {
+                (*qr)[j][i] = 1;
+                (*visited)[j][i] = 1;
+            }
+        }
+    }
+
+    int tam, tam2;
+
+    if (n % 2 == 0) 
+        tam = (n / 2); tam2 = (n / 2);
+    if (n % 2 != 0) 
+        tam = (n / 2); tam2 = (n / 2) + 1;
+
+    int aux1 = tam * tam2 , aux2 = tam * tam, aux3 = tam * tam2, aux4 = tam2 * tam2;
+    
+    //NOTE: FILL QUANDRANTS
+    if (qb[0] == aux1) {
+        for (int i = 0; i < tam; i++) {
+            for (int j = tam ; j < n; j++) {
+                if ((*visited)[i][j] == 0) {
+                    (*visited)[i][j] = 1;
+                    (*qr)[i][j] = 1;
+                }
+            }
+        }
+    } else if (qb[0] == 0) {
+        for (int i = 0; i < tam; i++) {
+            for (int j = tam ; j < n; j++) {
+                if ((*visited)[i][j] == 0) {
+                    (*visited)[i][j] = -1;
+                }
+            }
+        }
+    }
+    if (qb[1] == aux2) {
+        for (int i = 0; i < tam; i++) {
+            for (int j = 0 ; j < tam; j++) {
+                if ((*visited)[i][j] == 0) {
+                    (*visited)[i][j] = 1;
+                    (*qr)[i][j] = 1;
+                }
+            }
+        }
+    } else if (qb[1] == 0) {
+        for (int i = 0; i < tam; i++) {
+            for (int j = 0 ; j < tam; j++) {
+                if ((*visited)[i][j] == 0) {
+                    (*visited)[i][j] = -1;
+                }
+            }
+        }
+    }
+    if (qb[2] == aux3) {
+        for (int i = tam; i < n; i++) {
+            for (int j = 0 ; j < tam; j++) {
+                if ((*visited)[i][j] == 0) {
+                    (*visited)[i][j] = 1;
+                    (*qr)[i][j] = 1;
+                }
+            }
+        }
+    } else if (qb[2] == 0) {
+        for (int i = tam; i < n; i++) {
+            for (int j = 0 ; j < tam; j++) {
+                if ((*visited)[i][j] == 0) {
+                    (*visited)[i][j] = -1;
+                }
+            }
+        }
+    }
+    if (qb[3] == aux4) {
+        for (int i = tam; i < n; i++) {
+            for (int j = tam ; j < n; j++) {
+                if ((*visited)[i][j] == 0) {
+                    (*visited)[i][j] = 1;
+                    (*qr)[i][j] = 1;
+                }
+            }
+        }
+    } else if (qb[3] == 0) {
+        for (int i = tam; i < n; i++) {
+            for (int j = tam ; j < n; j++) {
+                if ((*visited)[i][j] == 0) {
+                    (*visited)[i][j] = -1;
+                }
+            }
+        }
+    }
+    
+    //NOTE: DIAGONALS
+    if(db[0] == 0){
+        for (int i = 0; i < n; i++) {
+            (*visited)[i][i] = -1;
+            (*qr)[i][i] = 0;       
+        }
+    }else if(db[0] == n){
+        for (int i = 0; i < n; i++){
+            (*visited)[i][i] = 1;
+            (*qr)[i][i] = 1;
+        }
+    }
+
+    if(db[1] == 0){
+        for (int i = 0; i < n; i++) {
+            int indice = n-i-1;
+            (*visited)[i][indice] = -1;
+            (*qr)[i][indice] = 0;       
+        }
+    }else if(db[1] == n){
+        for (int i = 0; i < n; i++){
+            int indice = n-i-1;
+            (*visited)[i][indice] = 1;
+            (*qr)[i][indice] = 1;
+        }
+    }
+
+
+    for (int i = 0; i < n; i++){
+        if((*visited)[i][i] == 1){
+            countDiagonal1++;
+        }
+    }
+    for (int i = 0; i < n; i++){
+        if((*visited)[i][i] == -1){
+            countDiagonal0++;
+        }
+    }
+    if (n - countDiagonal0 == db[0]) {
+        for (int i = 0; i < n; i++) {
+            if ((*visited)[i][i] == 0) {
+                (*visited)[i][i] = 1;
+                (*qr)[i][i] = 1;
+            }
+        }
+    }else if(countDiagonal1 == db[0]){
+        for (int i = 0; i < n; i++) {
+            if ((*visited)[i][i] == 0) {
+                (*visited)[i][i] = -1;
+                (*qr)[i][i] = 0;
+            }
+        }
+    }
+
+
+    for (int i = 0; i < n; i++){
+        int indice = n - i - 1;
+        if((*visited)[i][indice] == 1){
+            countDiagonalInv1++;
+        }
+    }
+    for (int i = 0; i < n; i++){
+        int indice = n - i - 1;
+        if((*visited)[i][indice] == -1){
+            countDiagonalInv0++;
+        }
+    }
+    if (n - countDiagonalInv0 == db[0]) {
+        for (int i = 0; i < n; i++) {
+            int indice = n - i - 1;
+            if ((*visited)[i][indice] == 0) {
+                (*visited)[i][indice] = 1;
+                (*qr)[i][indice] = 1;
+            }
+        }
+    }else if(countDiagonalInv1 == db[0]){
+        for (int i = 0; i < n; i++) {
+            int indice = n - i - 1;
+            if ((*visited)[i][indice] == 0) {
+                (*visited)[i][indice] = -1;
+                (*qr)[i][indice] = 0;
+            }
+        }
+    }
+    
+
+
+    //NOTE:
+    for (int i = 0; i < n; i++) {
+        if (n - countLines0 == cb[i]) {
+            for (int j = 0; j < n; j++) {
+                if ((*visited)[j][i] == 0) {
+                    (*visited)[j][i] = 1;
+                    (*qr)[j][i] = 1;
+                }
+            }
+        }
+        if (n - countLines1 == cb[i]) {
+            for (int j = 0; j < n; j++) {
+                if ((*visited)[j][i] == 0) {
+                    (*visited)[j][i] = -1;
+                    (*qr)[j][i] = 0;
+                }
+            }
+        }
+        if (n - countCols1 == lb[i]) {
+            for (int j = 0; j < n; j++) {
+                if ((*visited)[i][j] == 0) {
+                    (*visited)[i][j] = -1;
+                    (*qr)[i][j] = 0;
+                }
+            }
+        }
+        if (n - countCols0 == lb[i]) {
+            for (int j = 0; j < n; j++) {
+                if ((*visited)[i][j] == 0) {
+                    (*visited)[i][j] = 1;
+                    (*qr)[i][j] = 1;
+                }
+            }
+        }
+
+    }
+
+    idxLines.clear();
+    idxCols.clear();
+}
 
 void func(int n, vector<int> lb, vector<int> cb, vector<int> lt, vector<int> ct, vector<int> qb, vector<int> db) {
-    cout << "teste" << endl;
-    
-    node * root = new node(n);
-    //node * child1 = new node(n);
+    countqrs = 0;
+    int aux = 0;
+    vector<vector<int>> qr(n, vector<int>(n));
+    vector<vector<int>> visited(n, vector<int>(n));
 
-    //root->newChild(child1);
+    pre_proc(&qr, &visited, n, lb, cb, lt, ct, qb, db);
 
-    //child1->qr[0][0] = 1;
+    //TODO: Se a matriz visited estiver totalmente preenchida a 1 na entrar no rec!
 
-    
     for (int i = 0; i < n; i++) {
-        vector<int> line(n);
-
-        fill_line(root, line, n, lb[i], 0, i);
-
+        for (int j = 0; j < n; j++) {
+            if (visited[i][j] == 0) {
+                aux = 1;
+                break;
+            }
+        }
+        if (aux == 1)
+            break;
     }
-    cout << "teste2" << endl;
+
+    //TODO: Se o pre_proc retornar impossivel nao entrar no rec 
+    
+
+    if (aux == 1)
+        rec(qr, visited, n, 0, 0, lb, cb, lt, ct, qb, db);
+    else
+        saved = qr;
+
+    
+    if (countqrs == 1 || aux == 0) {
+        cout << "VALID: 1 QR Code generated!" << endl;
+        printQrCode(saved, n);
+    } else if (countqrs == 0 ) {
+        cout << "DEFECT: No QR Code generated!" << endl;
+    } else if (countqrs > 1) {
+        cout << "INVALID: " << countqrs << " QR Codes generated!" << endl;
+    }
 
 }
 
-void rec(node * root){
-
-    for (int i = 0; i < root->children.size(); i++) {
-        vector<int> line(n);
-
-        fill_line(root->children[i], line, n, lb[i], 0, i);
-
-    }
-
-}
-
-
-void fill_line(node * root, vector<int> vec, int n, int blacks, int i, int line) {
-
-    if (blacks == 0) {
-        node * child = new node(n);
-        child->qr = root->qr;
-        child->qr[line] = vec;
-        root->newChild(child);
-        
-        return;
-    }
-
-    for (int j = i; j < n; j++) {
-        vec[j] = 1;
-        fill_line(root, vec, n, blacks - 1, i + 1, line);
-        vec[j] = 0;
-    }
-}
 
 
 int main() {
+    
+    //clock_t tStart = clock();
 
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -120,6 +579,8 @@ int main() {
         qb.clear();
         db.clear();
     }
+
+    //printf("Time taken: %.10fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
 
     return 0;
 }
